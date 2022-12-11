ee1d650e8cdce4399de82a80252776777184ba1a
RFilho01 <robertocorreiafilho0100@outlook.com>
2022-12-11 16:47:15 -0300

    refactoring

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,7 @@ require 'pry'
 
 require_relative 'spec_helper/spec_helper'
 require_relative 'data/models/usuario_model'
+require_relative 'data/lib/factories'
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{ENV['AMBIENTE']}.yaml")
 
